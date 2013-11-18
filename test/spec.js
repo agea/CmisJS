8de7a863eaa61b767cda1534c824ddc0d41fbdbc
Andrea Agili <agea@drwolf.it>
2013-11-18 10:37:28 +0100

    library setup using superagent for ajax requests

--- test/spec.js
+++ test/spec.js
@@ -6,11 +6,9 @@
 
 'use strict'
 
-root.request = require('superagent');
 
 var assert = require('assert'),
-
-lib = require('../lib/cmis');
+	cmis = require('../lib/cmis').cmis;
 
 var url = "http://localhost:18080/alfresco/cmisbrowser";
 var username = "admin";
@@ -20,10 +18,29 @@ var password = "admin";
 describe('CmisJS library test', function() {
   
   it('should connect to a repository', function(done) {
-  	lib.cmis.connect(url, username, password).end(function(res){
+  	cmis.connect(url, username, password).ok(function(res){
+
+  		assert(parseFloat(cmis.repo.cmisVersionSupported)>=1.09,
+  			"CMIS Version should be at least 1.1");
   		assert(res.ok,"Response should be ok");
   		done();
   	});
   });
+
+  it('should retrieve an object by path', function(done) {
+  	cmis.getObjectByPath('/').ok(function(res){
+  		assert(res.body.succinctProperties['cmis:name'] !== undefined,'name should not be undefined');
+  		done();
+  	});
+  });
+
+  it('should non found this path', function(done) {
+  	cmis.getObjectByPath('/thispathshouldnotexist').notOk(function(res){
+  		assert(res.status === 404,'status should be 404');
+  		done();
+  	});
+  });
+
+
 
 })

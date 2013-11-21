7796896b3570bcfb8c2473d975bb4012b30b56ad
Andrea Agili <agea@drwolf.it>
2013-11-21 23:11:38 +0100

    API refactoring to be more similar to other CMIS implementations, added method signatures for all services

--- test/spec.js
+++ test/spec.js
@@ -1,36 +1,35 @@
 'use strict'
 
+var url = "http://cmis.alfresco.com/cmisbrowser";
+var username = "admin";
+var password = "admin";
+
+
 var assert = require('assert'),
 	cmis = require('../lib/cmis').cmis;
 
-var url = "http://localhost:18080/alfresco/cmisbrowser";
-var username = "admin";
-var password = "admin";
+if (process.argv.indexOf('--url')!=-1) {
+	url = process.argv[process.argv.indexOf('--url')+1];
+}
+
+var session = cmis.createSession(url);
 
 describe('CmisJS library test', function() {
   
   it('should connect to a repository', function(done) {
-  	cmis.connect(url, username, password).ok(function(res){
-
-  		assert(parseFloat(cmis.repo.cmisVersionSupported)>=.99,
-  			"CMIS Version should be at least 1.0");
-  		assert(res.ok,"Response should be ok");
-  		done();
-  	}).error(function(){
-  		cmis.connect("http://cmis.alfresco.com/cmisbrowser", username, password).ok(function(res){
-
-  		assert(parseFloat(cmis.repo.cmisVersionSupported)>=.99,
-  			"CMIS Version should be at least 1.0");
-  		assert(res.ok,"Response should be ok");
-  		done();
-  	})
-  	});
+  	session.setCredentials(username, password).loadRepositories()
+  		.ok(function(res){
+	  		assert(parseFloat(session.defaultRepository.cmisVersionSupported)>=.99,
+	  			"CMIS Version should be at least 1.0");
+	  		assert(res.ok,"Response should be ok");
+	  		done();
+	  		});
   });
 
   var rootId;
 
   it('should retrieve an object by path', function(done) {
-  	cmis.getObjectByPath('/').ok(function(res){
+  	session.getObjectByPath('/').ok(function(res){
   		rootId = res.body.succinctProperties['cmis:objectId'];
   		assert(res.body.succinctProperties['cmis:name'] !== undefined,'name should not be undefined');
   		done();
@@ -38,7 +37,7 @@ describe('CmisJS library test', function() {
   });
 
   it('should retrieve an object by id', function(done) {
-  	cmis.getObject(rootId).ok(function(res){
+  	session.getObject(rootId).ok(function(res){
   		rootId = res.body.succinctProperties['cmis:objectId'];
   		assert(res.body.succinctProperties['cmis:path'] == '/','root object path should be /');
   		done();
@@ -48,7 +47,7 @@ describe('CmisJS library test', function() {
   var randomFolder = "CmisJS" + Math.random();
 
   it('should non found this path', function(done) {
-  	cmis.getObjectByPath("/" + randomFolder).notOk(function(res){
+  	session.getObjectByPath("/" + randomFolder).notOk(function(res){
   		assert(res.status === 404,'status should be 404');
   		done();
   	});
@@ -56,7 +55,7 @@ describe('CmisJS library test', function() {
 
   var randomFolderId;
   it('should create a folder', function(done) {
-  	cmis.createFolder(rootId, randomFolder).ok(function(res){
+  	session.createFolder(rootId, randomFolder).ok(function(res){
   		randomFolderId = res.body.succinctProperties['cmis:objectId'];
   		assert(randomFolderId !== undefined,'objectId should be defined');
   		assert(res.status === 201,'status should be 201');
@@ -65,7 +64,7 @@ describe('CmisJS library test', function() {
   });
 
   it('should delete a folder', function(done) {
-  	cmis.deleteObject(randomFolderId, true).ok(function(res){
+  	session.deleteObject(randomFolderId, true).ok(function(res){
   		assert(res.status === 200,'status should be 200');
   		done();
   	});
